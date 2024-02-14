69a883f430153af8651f3501057d970c43cffdfe
Fckland <matt.fckland@gmail.com>
2024-02-14 10:23:04 -0300

    Google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TQ3F66XH"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
